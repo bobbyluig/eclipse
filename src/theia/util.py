0c8b5b5e03f24d3ce4fd614d6e93a76e5a2fe5de
Lujing Cen <lujingcen@gmail.com>
2015-12-27 23:53:59 -0800

    Fixed typos.

--- src/theia/util.py
+++ src/theia/util.py
@@ -4,8 +4,8 @@ import numpy as np
 def CIE76(lab_color_vector, lab_color_matrix):
     """
     Computes one or more Delta Es using CIE76.
-    :param lab_color_vector: A numpy array of the sample LAB color.
-    :param lab_color_matrix: A numpy vector of the predefined LAB colors.
+    :param lab_color_vector: A numpy vector of the sample LAB color.
+    :param lab_color_matrix: A numpy matrix of the predefined LAB colors.
     :return: A numpy array of CIE76 distances.
     """
 
