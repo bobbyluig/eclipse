1f1127f9390872d5897aae44689bf61f3d23b8fa
Lujing Cen <lujingcen@gmail.com>
2016-01-04 22:37:20 -0800

    Commit before switching

--- src/theia/oculus/src/cf_libs/line2d/line2d.hpp
+++ src/theia/oculus/src/cf_libs/line2d/line2d.hpp
@@ -6,6 +6,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <inpaint/template_match_candidates.h>
+
 #include <memory>
 #include <iostream>
 #include <fstream>
@@ -15,10 +17,10 @@
 #include <math.h>
 
 #include "gradientMex.hpp"
-#include "feature_maps.hpp"
 
 using namespace boost::python;
 using namespace std;
+using namespace Inpaint;
 
 #define PI 3.14159265f
 
@@ -27,7 +29,7 @@ namespace template_match
 	struct Line2DParameters
 	{
 		int numQuantizations = 8;
-		double threshold = 5.0;
+		float threshold = 5.0;
 		bool useGaussian = true;
 		int tau = 8;
 	};
@@ -35,18 +37,18 @@ namespace template_match
 	class Line2D
 	{
 	public:
-		typedef double T; // set precision here double or float
-
 		Line2D(Line2DParameters paras)
 			: _NUM_ORIENTS(paras.numQuantizations),
 			_THRESHOLD(paras.threshold),
 			_USE_GAUSSIAN(paras.useGaussian),
 			_TAU(paras.tau)
 		{
+			generateLookupTable();
 		}
 
-		void test(const cv::Mat& image)
+		void test(const cv::Mat& image, const cv::Mat& templ)
 		{
+			//image
 			cv::Mat floatImage;
 			image.convertTo(floatImage, CV_32FC3);
 
@@ -59,43 +61,155 @@ namespace template_match
 			else {
 				cvOri(floatImage, reinterpret_cast<int*>(bins.data), _NUM_ORIENTS);
 			}
+			
+			// cv::Mat map(image.size(), CV_32SC1);
+			// computeBinaryMap(bins, map);
 
-			cv::Mat map(image.size(), CV_32SC1);
+			// cv::Mat* responseMap = new cv::Mat[_NUM_ORIENTS];
+			// computeResponseMap(map, responseMap);
 
-			computeBinaryMap(bins, map);
+			// template
+			cv::Mat floatTemplate;
+			templ.convertTo(floatTemplate, CV_32FC3);
 
-			cout << map << endl;
+			cv::Mat templBins(templ.size(), CV_32SC1);
+
+			cv::GaussianBlur(floatTemplate, floatTemplate, cv::Size(11, 11), 0);
+			cvOriNone(floatTemplate, reinterpret_cast<int*>(templBins.data), _NUM_ORIENTS);
+
+			// output
+			// cv::Mat output(image.size(), CV_32FC1);
+			// compare(responseMap, templBins, output);
+
+			bins.convertTo(bins, CV_8U);
+			templBins.convertTo(templBins, CV_8U);
+
+			cv::Mat result;
+			cv::matchTemplate(bins, templBins, result, CV_TM_CCOEFF_NORMED);
+
+			cv::imshow("map", result);
 		}
 
-		void test2()
+		void test2(const cv::Mat& image, const cv::Mat& templ)
 		{
-			cout << findCosMax(195, 8) << endl;
+			cv::Mat dst;
+			fastMatchTemplate(image, templ, dst, 3);
+			cv::imshow("fast", dst);
 		}
 		
 	private:
+		void fastMatchTemplate(const cv::Mat& srca,  // The reference image
+			const cv::Mat& srcb,  // The template image
+			cv::Mat& dst,   // Template matching result
+			int maxlevel)   // Number of levels
+		{
+			std::vector<cv::Mat> refs, tpls, results;
+
+			// Build Gaussian pyramid
+			cv::buildPyramid(srca, refs, maxlevel);
+			cv::buildPyramid(srcb, tpls, maxlevel);
+
+			cv::Mat ref, tpl, res;
+
+			// Process each level
+			for (int level = maxlevel; level >= 0; level--)
+			{
+				ref = refs[level];
+				tpl = tpls[level];
+				res = cv::Mat::zeros(ref.size() + cv::Size(1, 1) - tpl.size(), CV_32FC1);
+
+				if (level == maxlevel)
+				{
+					// On the smallest level, just perform regular template matching
+					cv::matchTemplate(ref, tpl, res, CV_TM_CCOEFF_NORMED);
+				}
+				else
+				{
+					// On the next layers, template matching is performed on pre-defined 
+					// ROI areas.  We define the ROI using the template matching result 
+					// from the previous layer.
+
+					cv::Mat mask;
+					cv::pyrUp(results.back(), mask);
+
+					cv::Mat mask8u;
+					mask.convertTo(mask8u, CV_8U);
+
+					// Find matches from previous layer
+					std::vector<std::vector<cv::Point> > contours;
+					cv::findContours(mask8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+					// Use the contours to define region of interest and 
+					// perform template matching on the areas
+					for (int i = 0; i < contours.size(); i++)
+					{
+						cv::Rect r = cv::boundingRect(contours[i]);
+						cv::matchTemplate(
+							ref(r + (tpl.size() - cv::Size(1, 1))),
+							tpl,
+							res(r),
+							CV_TM_CCORR_NORMED
+							);
+					}
+				}
+
+				// Only keep good matches
+				cv::threshold(res, res, 0.8, 1., CV_THRESH_TOZERO);
+				results.push_back(res);
+			}
+
+			res.copyTo(dst);
+		}
+
+		void compare(cv::Mat* responseMap, cv::Mat& templateBins, cv::Mat& output) {
+			const int width = templateBins.cols;
+			const int height = templateBins.rows;
+
+			for (int i = 0; i < output.rows - height; ++i) {
+				for (int j = 0; j < output.cols - width; ++j) {
+					float score = 0;
+
+					for (int tH = 0; tH < templateBins.rows; ++tH) {
+						for (int tW = 0; tW < templateBins.cols; ++tW) {
+							if (templateBins.at<int>(tH, tW) != 0)
+								score += responseMap[templateBins.at<int>(tH, tW) - 1].at<float>(i + tH, j + tW);
+						}
+					}
+
+					output.at<float>(i, j) = score / width / height;
+				}
+			}
+		}
+
+		// populate lookup table
+		// each row is a new bin
 		void generateLookupTable()
 		{
-			const int size = (1 << _TAU) - 1;
+			const int cols = 1 << _NUM_ORIENTS;
 
 			// allocate space for table
-			_lookupTable.reserve(size * _TAU);
+			_lookupTable = cv::Mat(_NUM_ORIENTS, cols, CV_32FC1);
 
-
+			for (int row = 0; row < _NUM_ORIENTS; ++row) {
+				for (int col = 0; col < cols; ++col) {
+					_lookupTable.at<float>(row, col) = findCosMax(col, row + 1);
+				}
+			}
 		}
 
 		// the world's worst function
 		// it's okay, computed offline :p
-		T findCosMax(int cell, int bin)
+		float findCosMax(int cell, int bin)
 		{
-			bool* a = new bool[_TAU];
-			T max = 0;
+			bool* a = new bool[_NUM_ORIENTS];
+			float max = 0;
 
-			for (int i = _TAU - 1; i >= 0; --i)
+			for (int i = _NUM_ORIENTS - 1; i >= 0; --i)
 				a[i] = (cell >> i) & 1;
 			
-			for (int i = 0; i < _TAU; ++i) {
+			for (int i = 0; i < _NUM_ORIENTS; ++i) {
 				if (a[i]) {
-					T similarity;
+					float similarity;
 					similarity = (1 << i) * _radsPerBin;
 					similarity = abs(cos(similarity - (bin * _radsPerBin)));
 
@@ -107,6 +221,20 @@ namespace template_match
 			delete[] a;
 
 			return max;
+		}
+
+		void computeResponseMap(cv::Mat& in, cv::Mat* outArray)
+		{
+			const int width = 1 << _NUM_ORIENTS;
+
+			for (int bin = 0; bin < _NUM_ORIENTS; ++bin) {
+				outArray[bin] = cv::Mat(in.size(), CV_32FC1);
+
+				for (int i = 0; i < in.cols * in.rows; ++i) {
+					outArray[bin].at<float>(i) = _lookupTable.at<float>(bin, in.at<int>(i));
+				}
+			}
+
 		}
 
 		void computeBinaryMap(cv::Mat& bins, cv::Mat& out)
@@ -248,12 +376,13 @@ namespace template_match
 		}
 
 		const int _NUM_ORIENTS;
-		const T _THRESHOLD;
+		const float _THRESHOLD;
 		const bool _USE_GAUSSIAN;
 		const int _TAU;
 
-		vector<int> _lookupTable;
-		const T _radsPerBin = PI / _NUM_ORIENTS;
+		cv::Mat _lookupTable;
+		const float _radsPerBin = PI / _NUM_ORIENTS;
+		TemplateMatchCandidates _matcher;
 	};
 }
 
