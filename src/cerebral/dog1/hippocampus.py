b4a670be70156c2c561dacbf9c8d25de7e2bc898
Lujing Cen <lujingcen@gmail.com>
2016-02-02 12:18:07 -0800

    last imute!!!

--- src/cerebral/dog1/hippocampus.py
+++ src/cerebral/dog1/hippocampus.py
@@ -8,7 +8,7 @@ import os
 
 class Android:
     # Leg 1.
-    servo1 = Servo(0, -180, 90, 500, 2500, 150, bias=-10, direction=1)
+    servo1 = Servo(0, -180, 90, 500, 2500, 150, bias=-13, direction=1)
     servo2 = Servo(1, -45, 225, 500, 2500, 150, bias=0, direction=1)
     servo3 = Servo(2, -135, 135, 500, 2500, 150, bias=0, direction=-1)
     leg1 = Leg(servo1, servo2, servo3, (7.5, 7.5), 0)
@@ -16,19 +16,19 @@ class Android:
     # Leg 2.
     servo4 = Servo(3, -90, 180, 500, 2500, 150, bias=10, direction=-1)
     servo5 = Servo(4, -225, 45, 500, 2500, 150, bias=0, direction=1)
-    servo6 = Servo(5, -135, 135, 500, 2500, 150, bias=0, direction=1)
+    servo6 = Servo(5, -135, 135, 500, 2500, 150, bias=10, direction=1)
     leg2 = Leg(servo4, servo5, servo6, (7.5, 7.5), 1)
 
     # Leg 3.
-    servo7 = Servo(6, -90, 180, 500, 2500, 150, bias=0, direction=1)
-    servo8 = Servo(7, -45, 225, 500, 2500, 150, bias=0, direction=1)
+    servo7 = Servo(6, -90, 180, 500, 2500, 150, bias=-10, direction=1)
+    servo8 = Servo(7, -45, 225, 500, 2500, 150, bias=2, direction=1)
     servo9 = Servo(8, -135, 135, 500, 2500, 150, bias=0, direction=-1)
     leg3 = Leg(servo7, servo8, servo9, (7.5, 7.5), 2)
 
     # Leg 4 .
     servo10 = Servo(9, -180, 90, 500, 2500, 150, bias=10, direction=-1)
-    servo11 = Servo(10, -225, 45, 500, 2500, 150, bias=0, direction=1)
-    servo12 = Servo(11, -135, 135, 500, 2500, 150, bias=-10, direction=1)
+    servo11 = Servo(10, -225, 45, 500, 2500, 150, bias=-3, direction=1)
+    servo12 = Servo(11, -135, 135, 500, 2500, 150, bias=-8, direction=1)
     leg4 = Leg(servo10, servo11, servo12, (7.5, 7.5), 3)
 
     # Robot.
