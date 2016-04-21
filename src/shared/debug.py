d519abf5a7349b8e5ef98823d767d85966ed52f6
Lujing Cen <lujingcen@gmail.com>
2016-04-21 12:40:52 -0700

    better dummy function

--- src/shared/debug.py
+++ src/shared/debug.py
@@ -3,11 +3,14 @@ class Dummy:
     Implements a dummy class that is completely inert.
     """
 
-    def dummy_function(*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         pass
 
     def __getattr__(self, item):
         return self.dummy_function
 
     def __setattr__(self, key, value):
+        pass
+
+    def dummy_function(*args, **kwargs):
         pass
