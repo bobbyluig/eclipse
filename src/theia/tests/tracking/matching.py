545d83ab50f3e55f733b4f4b725494d0d9a59cdc
Lujing Cen <lujingcen@gmail.com>
2016-05-25 09:29:26 -0700

    sync

--- src/theia/tests/tracking/matching.py
+++ src/theia/tests/tracking/matching.py
@@ -2,8 +2,8 @@ import cv2
 from theia.matcher import LineMatcher
 
 matcher = LineMatcher()
-duck = cv2.imread('duck.jpg')
-template = cv2.imread('roi.jpg')
+duck = cv2.imread('theia/src/tests/tracking/duck.jpg')
+template = cv2.imread('theia/src/tests/tracking/roi.jpg')
 
 
 id = matcher.add_template(template, 'duck')
